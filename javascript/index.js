3270141802dbc3ce531a7500207fbc1f6ac9747a
cee-tech21 <easyoandi@gmail.com>
2022-07-21 06:18:56 +0100

    add more api data to dom

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,9 @@ let allPosts = theData;
 console.log(allPosts[0]);
 // console.log(allPosts[0].canonical_url)
 console.log(allPosts[0].content.rendered)
-console.log("image url: ", allPosts[0].jetpack_featured_media_url)
-console.log("the array length", allPosts.length)
+// console.log("image url: ", allPosts[0].jetpack_featured_media_url)
+//console.log("the array length", allPosts.length)
+
 
 
 // Target DOM elements
@@ -17,10 +18,10 @@ theHeadline.innerHTML = allPosts[0].excerpt.rendered
 let featuredNews = getElement("#featured-News")
 
 for(let i = 1; i < allPosts.length; i++){
-    console.log(allPosts[i].excerpt.rendered)
+    // console.log(allPosts[i].excerpt.rendered)
     featuredNews.innerHTML += `<article class="card">
-                                <img src="./assets/eifel_tower.jpg" 
-                                    class="featured__image">
+                                <img src="${allPosts[i].jetpack_featured_media_url}" 
+                                    class="featured__image" alt="article image">
                                 ${allPosts[i].excerpt.rendered}
     
                             </article>`
